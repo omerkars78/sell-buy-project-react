df63f2c037e8474bd5706aba3273518bc42f1edb
Ömer Kars <81978907+omerkars78@users.noreply.github.com>
2023-11-22 23:50:51 +0300

    work to profile image select

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,9 @@
 // components/Register.tsx
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { signup } from '../services/authService';
-import { Button, TextField,  Typography, Container, Grid } from '@mui/material';
-
+import { Button, TextField, Typography, Container, Grid, IconButton } from '@mui/material';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import EditIcon from '@mui/icons-material/Edit'; 
 interface SignupData {
     name: string;
     surname: string;
@@ -37,6 +38,20 @@ const Register: React.FC = () => {
     const [detailedAddress, setDetailedAddress] = useState('');
     const [error, setError] = useState<string | null>(null);
 
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+    const handleProfileImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files[0]) {
+            setProfileImage(URL.createObjectURL(event.target.files[0]));
+        }
+    };
+
+    const handleIconClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
@@ -68,12 +83,52 @@ const Register: React.FC = () => {
 
     return (
         <Container maxWidth="md" style={{ marginTop: '40px' }}>
-             <Typography component="h1" variant="h5" style={{ textAlign: 'center' }}>
-                    Register
-                </Typography>
+              
             <form onSubmit={handleSubmit}>
-                <Grid container spacing={2}>
+            <Grid container spacing={2} justifyContent="center">
+                <Grid item xs={12} style={{ textAlign: 'center' }}>
+                        {/* Circular Container for Profile Image */}
+                        <div style={{
+                            margin: 'auto',
+                            width: '150px',
+                            height: '150px',
+                            borderRadius: '50%',
+                            position: 'relative',
+                            overflow: 'hidden',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            backgroundColor: '#f0f0f0'
+                        }}>
+                            {profileImage && <img src={profileImage} alt="Profile" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />}
+                            <input
+                                accept="image/*"
+                                style={{ display: 'none' }}
+                                id="profile-image-file"
+                                type="file"
+                                onChange={handleProfileImageChange}
+                                ref={fileInputRef}
+                            />
+                            <label htmlFor="profile-image-file">
+                                <IconButton color="primary" aria-label="upload picture" component="span" onClick={handleIconClick} style={{
+                                    position: 'absolute',
+                                    bottom: '0',
+                                    right: '0',
+                                    transform: 'translate(50%, 50%)',
+                                    backgroundColor: 'white',
+                                    borderRadius: '50%',
+                                    boxShadow: '0 2px 4px rgba(0,0,0,0.25)',
+                                    padding: '10px',
+                                    zIndex: 2, // Ensure it's on top
+                                    color: 'black' // Ensure the icon is visible
+                                }}>
+                                    <EditIcon />
+                                </IconButton>
+                            </label>
+                        </div>
+                    </Grid>
                     <Grid item xs={12} md={6}>
+                        
                         {/* Sol sütundaki inputlar */}
                         <TextField fullWidth label="Name" value={name} onChange={(e) => setName(e.target.value)} required margin="normal" />
                         <TextField fullWidth label="Surname" value={surname} onChange={(e) => setSurname(e.target.value)} required margin="normal" />
